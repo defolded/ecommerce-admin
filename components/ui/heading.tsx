9e6ca95e5c6515fb20a9e561ca6e45b8c88113ac
Nikita <hailrake04@gmail.com>
2023-11-17 22:31:43 -0500

    added remove and rename functionality in store settings

---
+++ components/ui/heading.tsx
@@ -0,0 +1,20 @@
+interface IHeading {
+    title: string
+    description: string
+}
+
+const Heading: React.FC<IHeading> = ({
+    title,
+    description
+}) => {
+  return (
+    <div>
+        <h2 className="text-3xl font-bold tracking-light">{title}</h2>
+        <p className="text-sm text-muted-foreground">
+            {description}
+        </p>
+    </div>
+  )
+}
+
+export default Heading
